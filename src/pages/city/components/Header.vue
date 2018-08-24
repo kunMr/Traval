f754975574b28891d8f25ea2f81820f6edec5360
kunMr <1553069352@qq.com>
2018-08-24 14:07:27 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    background: $bgColor
+    font-size: .32rem
+    color: #fff
+    .back-icon
+      position: absolute
+      left: 0
+      top: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
